7c66578bb409cb1499a91b1131eaeffc604b913c
MatveyPrybyshchuk <matvey.pr.v@gmail.com>
2026-03-16 20:44:26 +0300

    Updated homework 5

--- lesson.py
+++ lesson.py
@@ -18,9 +18,17 @@
 """
 
 
-def is_palindrome(word: str) -> str:
+def is_palindrome_1(word: str) -> bool:
     word = word.lower().strip()
     return word == word[::-1]
 
+def is_palindrome_2(word: str) -> bool:
+    word = word.lower().strip()
+    for i in range(0, len(word) // 2):
+        if word[i] != word[-i - 1]:
+            return False
+    return True
 
-print(is_palindrome("Pap "))
+
+print(is_palindrome_1("Pap "))
+print(is_palindrome_2("Pap"))
