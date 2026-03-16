07b8293b7e36db22dd86aaa10f93714b3e0ffae5
MatveyPrybyshchuk <matvey.pr.v@gmail.com>
2026-03-16 20:53:02 +0300

    Uncommit

--- homework_4.py
+++ homework_4.py
@@ -1,40 +1,40 @@
 from prints.prints_homework import task_visualization
 
 
-# task = "Дано целое число. Если оно является положительным, то прибавить к нему 1; " \
-# "в противном случае не изменять его. Вывести полученное число"
-# task_visualization(1, task)
+task = "Дано целое число. Если оно является положительным, то прибавить к нему 1; " \
+"в противном случае не изменять его. Вывести полученное число"
+task_visualization(1, task)
 
-# num = int(input("Insert num for calculation: "))
-# print(num + 1 if num > 0 else num )
-
-
-
-# task = "Даны три целых числа. Найти количество положительных чисел в исходном наборе."
-# task_visualization(2, task)
-
-# nums = input("Insert 3 num in a row using space: ")
-
-# nums_list = list(map(int, nums.split()))
-
-# result = sum(1 for x in nums_list if x > 0)
-# print(result)
+num = int(input("Insert num for calculation: "))
+print(num + 1 if num > 0 else num )
 
 
-# task = "Дан номер года (положительное целое число). Определить количество дней " \
-# "в этом году, учитывая, что обычный год насчитывает 365 дней, а високосный — 366 дней. " \
-# "Високосным считается год, делящийся на 4, за исключением тех годов, которые" \
-# "делятся на 100 и не делятся на 400 (например, годы 300, 1300 и 1900 не являются" \
-# "високосными, а 1200 и 2000 являются)."
-# task_visualization(3, task)
 
-# year = int(input("Insert year for calculations: "))
+task = "Даны три целых числа. Найти количество положительных чисел в исходном наборе."
+task_visualization(2, task)
 
-# days = 365
-# if year % 4 == 0 and not (year % 400 != 0 and year % 100 == 0):
-#     days += 1
+nums = input("Insert 3 num in a row using space: ")
 
-# print(days)
+nums_list = list(map(int, nums.split()))
+
+result = sum(1 for x in nums_list if x > 0)
+print(result)
+
+
+task = "Дан номер года (положительное целое число). Определить количество дней " \
+"в этом году, учитывая, что обычный год насчитывает 365 дней, а високосный — 366 дней. " \
+"Високосным считается год, делящийся на 4, за исключением тех годов, которые" \
+"делятся на 100 и не делятся на 400 (например, годы 300, 1300 и 1900 не являются" \
+"високосными, а 1200 и 2000 являются)."
+task_visualization(3, task)
+
+year = int(input("Insert year for calculations: "))
+
+days = 365
+if year % 4 == 0 and not (year % 400 != 0 and year % 100 == 0):
+    days += 1
+
+print(days)
 
 
 
@@ -53,82 +53,82 @@ print(result)
 
 
 
-# task = "Найти произведение положительных, сумму и количество отрицательных" \
-# "из 10 введенных целых значений. (Функция ввода с клавиатуры - input())"
-# task_visualization(5, task)
+task = "Найти произведение положительных, сумму и количество отрицательных" \
+"из 10 введенных целых значений. (Функция ввода с клавиатуры - input())"
+task_visualization(5, task)
 
-# inputs = input("Insert 10 nums using space: ")
-# inputs_arr = list(map(int, inputs.split()))
+inputs = input("Insert 10 nums using space: ")
+inputs_arr = list(map(int, inputs.split()))
 
-# multipl, neg_sum, neg_count = 1, 0, 0
+multipl, neg_sum, neg_count = 1, 0, 0
 
-# for i in inputs_arr:
-#     if i > 0: 
-#         multipl *= i
-#     elif i < 0: 
-#         neg_count += 1
-#         neg_sum += i
-
-
-# print(f"Произведение положительных: {multipl if multipl != 1 else 0}, "
-#       f"Сумма отрицательных: {neg_sum}, "
-#       f"Количество отрицательных: {neg_count}")
+for i in inputs_arr:
+    if i > 0: 
+        multipl *= i
+    elif i < 0: 
+        neg_count += 1
+        neg_sum += i
 
 
-
-# task = "Дано число N. Найти произведение всех чисел от 0 до N."
-# task_visualization(6, task)
-
-# num = int(input("Insert num N: "))
-
-# result = 1
-# for i in range (1, num):
-#     result *= i
-
-# print(result)
+print(f"Произведение положительных: {multipl if multipl != 1 else 0}, "
+      f"Сумма отрицательных: {neg_sum}, "
+      f"Количество отрицательных: {neg_count}")
 
 
 
-# task = "Поле засеяли цветами двух сортов на площади S1 и S2. Каждый год "\
-# " площадь цветов первого сорта увеличивается вдвое, а площадь второго сорта "\
-# "увеличивается втрое. Через сколько лет площадь первых сортов будет "\
-# "составлять меньше 10% от площади вторых сортов."
-# task_visualization(7, task)
+task = "Дано число N. Найти произведение всех чисел от 0 до N."
+task_visualization(6, task)
 
-# area_S1, area_S2 = map(int, input("Insert S1 and S2 using space: ").split())
+num = int(input("Insert num N: "))
 
-# if area_S1 <= 0.1 * area_S2:
-#     print(0)
-# else:
-#     counter = 0
-#     while area_S1 > 0.1 * area_S2:
-#         area_S1 *= 2
-#         area_S2 *= 3
-#         counter += 1
-#     print(counter)
+result = 1
+for i in range (1, num):
+    result *= i
+
+print(result)
 
 
 
-# task = "Напишите алгоритм для определения, является ли число n счастливым. " \
-# "Счастливое число определяется следующим процессом: " \
-# "Начиная с любого положительного целого числа, замените число суммой квадратов его цифр. " \
-# "Повторяйте процесс до тех пор, пока число не станет равным 1 (на этом значении оно и останется), " \
-# "или пока не замкнется в бесконечном цикле, не включающем 1. Числа, для которых этот процесс заканчивается на 1, " \
-# "являются счастливыми. Верните true, если n — счастливое число, и false, если нет."
-# task_visualization(8, task)
+task = "Поле засеяли цветами двух сортов на площади S1 и S2. Каждый год "\
+" площадь цветов первого сорта увеличивается вдвое, а площадь второго сорта "\
+"увеличивается втрое. Через сколько лет площадь первых сортов будет "\
+"составлять меньше 10% от площади вторых сортов."
+task_visualization(7, task)
 
-# num = int(input("Insert num: "))
+area_S1, area_S2 = map(int, input("Insert S1 and S2 using space: ").split())
 
-# def isLucky(num):
-#     history = set()
-#     while num != 1:
-#         if num in history:
-#             return False
-#         history.add(num)
+if area_S1 <= 0.1 * area_S2:
+    print(0)
+else:
+    counter = 0
+    while area_S1 > 0.1 * area_S2:
+        area_S1 *= 2
+        area_S2 *= 3
+        counter += 1
+    print(counter)
 
-#         num = sum(int(i)**2 for i in str(num))
-#     return True
+
+
+task = "Напишите алгоритм для определения, является ли число n счастливым. " \
+"Счастливое число определяется следующим процессом: " \
+"Начиная с любого положительного целого числа, замените число суммой квадратов его цифр. " \
+"Повторяйте процесс до тех пор, пока число не станет равным 1 (на этом значении оно и останется), " \
+"или пока не замкнется в бесконечном цикле, не включающем 1. Числа, для которых этот процесс заканчивается на 1, " \
+"являются счастливыми. Верните true, если n — счастливое число, и false, если нет."
+task_visualization(8, task)
+
+num = int(input("Insert num: "))
+
+def isLucky(num):
+    history = set()
+    while num != 1:
+        if num in history:
+            return False
+        history.add(num)
+
+        num = sum(int(i)**2 for i in str(num))
+    return True
        
-# print (isLucky(num))
+print (isLucky(num))
 
 
